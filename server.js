1c69f9633ad42cb0b5faa826c92b8bcd3910d449
Mona Lisa <limn64@yahoo.com>
2018-04-29 22:25:44 -0700

    add all

--- server.js
+++ server.js
@@ -19,12 +19,21 @@ app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/website/index.html')
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {quotes: result})
+    //res.sendFile(__dirname + '/website/index.html')
+  })
 })
 
 app.post('/quotes', (req, res) => {
-    console.log('hello')
+    db.collection('quotes').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+        console.log('saved to database')
+        res.redirect('/')
+    })
 })
+
 
 /*
 app.get('/', (req, res) => {
